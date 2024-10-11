c71fec12aae7a23f5dfe92e6fa99a50929dcd2ee
Bala <balakrishna.malipeddi@gmail.com>
2024-10-11 13:31:53 +0530

    Added initial Order Service implementation

---
+++ src/main/java/com/myshow4all/ecms_order_service/EcmsOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myshow4all.ecms_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmsOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmsOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myshow4all/ecms_order_service/EcmsOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myshow4all.ecms_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmsOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
